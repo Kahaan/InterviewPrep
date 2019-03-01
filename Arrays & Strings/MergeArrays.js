f7f915773d853c09e7b84fec6ceb7c14fc2afd72
Kahaan <kahaan.r.patel@gmail.com>
2019-03-01 12:46:21 -0800

    interviewCake practice

--- Arrays & Strings/MergeArrays.js
+++ Arrays & Strings/MergeArrays.js
@@ -1,4 +1,17 @@
 function mergeArrays(myArray, alicesArray) {
-  let result = myArray.concat(alicesArray).sort();
+  let result = [];
+  let shortestArr = myArray.length > alicesArray.length ? alicesArray : myArray;
+  let longestArr = myArray.length > alicesArray.length ? myArray : alicesArray;
+  let i = 0;
+  while (i < shortestArr.length) {
+    console.log(myArray[i], alicesArray[i]);
+    if (myArray[i] <= alicesArray[i]) {
+      result.push(myArray[i], alicesArray[i]);
+    } else if (myArray[i] > alicesArray[i]) {
+      result.push(alicesArray[i], myArray[i]);
+    }
+    i++;
+  }
+  return result.concat(longestArr.slice(i, longestArr.length));
   return result;
 }
