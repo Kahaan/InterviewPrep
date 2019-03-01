a0271d404b036eee0108534f45d3c5d68c72b166
Kahaan <kahaan.r.patel@gmail.com>
2019-03-01 10:09:40 -0800

    interviewCake practice

--- Arrays & Strings/InPlaceReverse.js
+++ Arrays & Strings/InPlaceReverse.js
@@ -5,8 +5,10 @@ function reverse(arrayOfChars) {
   while (i < length) {
     currChar = arrayOfChars.splice(i, 1);
     arrayOfChars.unshift(...currChar);
-    console.log(arrayOfChars);
     i += 1;
   }
   return arrayOfChars;
 }
+
+// TODO:  Since unshift is O(n) and is within the loop, and the loop is a different length than the arrayOfChars,
+// Check if the problem is O(ab) or O(n^2)
