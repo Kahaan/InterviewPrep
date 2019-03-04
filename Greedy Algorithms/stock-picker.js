dbf4123cfaf09edea4c15e06882d1fb18b23236f
Kahaan <kahaan.r.patel@gmail.com>
2019-03-03 16:57:26 -0800

    interviewCake practice

--- Greedy Algorithms/stock-picker.js
+++ Greedy Algorithms/stock-picker.js
@@ -3,8 +3,9 @@ function getMaxProfit(stockPrices) {
 
   for (let i = 0; i < stockPrices.length; i++) {
     let subset = stockPrices.slice(0, i + 1);
-    let high = Math.max(subset);
-    let low = Math.min(subset);
+    let high = Math.max(...subset);
+    let low = Math.min(...subset);
+
     let currMax = high - low;
     if (currMax > maxProfit) {
       maxProfit = currMax;
