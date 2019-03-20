d86762f0151c63e2233fb7d7b18453f9afa26395
Kahaan <kahaan.r.patel@gmail.com>
2019-03-20 12:46:48 -0700

    interview prep

--- graphs and trees/dfs.js
+++ graphs and trees/dfs.js
@@ -1,6 +1,9 @@
-function dfs(tree, target) {
-  // add current node val to arr,
-  // check if left exists, if so dfs on left with curr arr
-  // if left is nul, dfs on right with curr arr
-  // return curr arr once iterated through all nodes
+function dfs(root, target) {
+  if (root.value === null) return null;
+  if (root.value === target) return root;
+  root.children.forEach(child => {
+    let search_result = dfs(child, target);
+    if (search_result !== null) return search_result;
+  });
+  return null;
 }
