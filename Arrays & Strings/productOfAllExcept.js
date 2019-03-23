ad4a51f11c654b7113a3aa2840d0285f752d01ad
Kahaan <kahaan.r.patel@gmail.com>
2019-03-23 01:02:55 -0700

    interview prep

--- Arrays & Strings/productOfAllExcept.js
+++ Arrays & Strings/productOfAllExcept.js
@@ -1,15 +1,23 @@
-function getProductsOfAllIntsExceptAtIndex(arr) {
-  if (arr === []) return [];
-  let prod = arr.reduce((acc, el) => acc * el);
-  let arrWithoutZero = arr.filter(el => el !== 0);
-  let prodWithoutZero = arrWithoutZero.reduce((acc, el) => acc * el);
+function getProductsOfAllIntsExceptAtIndex(intArray) {
+  if (intArray.length < 2) {
+    throw new Error(
+      "Getting the product of numbers at other indices requires at least 2 numbers"
+    );
+  }
 
-  let result = arr.map((el, i) => {
-    if (el !== 0) {
-      return prod / el;
-    } else {
-      return prodWithoutZero;
-    }
-  });
-  return result;
+  const productsOfAllIntsExceptAtIndex = [];
+
+  let productSoFar = 1;
+  for (let i = 0; i < intArray.length; i++) {
+    productsOfAllIntsExceptAtIndex[i] = productSoFar;
+    productSoFar *= intArray[i];
+  }
+
+  productSoFar = 1;
+  for (let j = intArray.length - 1; j >= 0; j--) {
+    productsOfAllIntsExceptAtIndex[j] *= productSoFar;
+    productSoFar *= intArray[j];
+  }
+
+  return productsOfAllIntsExceptAtIndex;
 }
