e00fa4774785f14a0e4595761f8ba80c78e177f9
Kahaan <kahaan.r.patel@gmail.com>
2019-04-12 11:54:06 -0700

    Algopractice

--- Arrays & Strings/quadruplets.js
+++ Arrays & Strings/quadruplets.js
@@ -1,5 +1,8 @@
 // BRUTE FORCE SOLUTION:
 
+// Time complexity: O(n^4)
+// Space complexity: O(1)
+
 function quad(arr, s) {
   arr = arr.sort();
   for (let i = 0; i < arr.length - 3; i++) {
@@ -15,3 +18,27 @@ function quad(arr, s) {
   }
   return [];
 }
+
+// BETTER APPROACH:
+function quad(arr, s) {
+  if (arr.length < 4) return [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let balance = s - arr[i] - arr[j];
+      let low = j + 1;
+      let high = arr.length - 1;
+      while (low < high) {
+        if (arr[low] + arr[high] === balance) {
+          return [arr[i], arr[j], arr[low], arr[high]];
+        } else if (arr[low] + arr[high] < balance) {
+          low++;
+        } else {
+          high++;
+        }
+      }
+    }
+  }
+  return [];
+}
+
+quad([1, 2, 3, 4, 5], 10);
