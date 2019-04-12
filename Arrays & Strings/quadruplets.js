2ed89162d893e2eec4550014ce5529a27b5d8874
Kahaan <kahaan.r.patel@gmail.com>
2019-04-12 11:43:34 -0700

    Algopractice

--- Arrays & Strings/quadruplets.js
+++ Arrays & Strings/quadruplets.js
@@ -1,9 +1,11 @@
+// BRUTE FORCE SOLUTION:
+
 function quad(arr, s) {
   arr = arr.sort();
-  for (let i = 0; i < arr.length - 4; i++) {
-    for (let j = i + 1; j < arr.length - 3; j++) {
-      for (var k = 0; k < arr.length; k++) {
-        for (var l = 0; l < arr.length; l++) {
+  for (let i = 0; i < arr.length - 3; i++) {
+    for (let j = i + 1; j < arr.length - 2; j++) {
+      for (var k = j + 1; k < arr.length - 1; k++) {
+        for (var l = k + 1; l < arr.length; l++) {
           if (arr[i] + arr[j] + arr[k] + arr[l] === s) {
             return [arr[i], arr[j], arr[k], arr[l]];
           }
