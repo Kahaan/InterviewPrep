9f07996cc895c6f104145b99ee6eafe085701355
Kahaan <kahaan.r.patel@gmail.com>
2019-04-12 11:37:25 -0700

    Algopractice

--- Arrays & Strings/quadruplets.js
+++ Arrays & Strings/quadruplets.js
@@ -1,39 +1,15 @@
-// function findArrayQuadruplet(arr, s) {
-//   let result = [];
-//
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = i + 1; i < arr.length; j++) {
-//       for (let k = j + 1; k < arr.length; k++) {
-//         for (let l = k + 1; l < arr.length; l++) {
-//           if (arr[i] + arr[j] + arr[k] + arr[l] === s) {
-//             result.push(arr[i], arr[j], arr[k], arr[l]);
-//           }
-//         }
-//       }
-//     }
-//   }
-//   return result.sort((a, b) => a - b);
-// }
-
-function quad(arr, t) {
-  if (arr.length < 4) return [];
+function quad(arr, s) {
   arr = arr.sort();
-  let result = [];
-  for (let i = 0; i < arr.length - 2; i++) {
-    for (let j = i + 1; j < arr.length - 1; j++) {
-      for (let k = j + 1; k < arr.length; k++) {
-        let balance = s - arr[i] + arr[j] + arr[k];
-        let next = arr[k + 1];
-        let prev = arr[i - 1] || arr[i];
-        if (balance + next === t) {
-          return [arr[i], arr[j], arr[k], arr[k + 1]];
-        } else if (balance + next < t) {
-          i++;
-          j++;
-          k++;
+  for (let i = 0; i < arr.length - 4; i++) {
+    for (let j = i + 1; j < arr.length - 3; j++) {
+      for (var k = 0; k < arr.length; k++) {
+        for (var l = 0; l < arr.length; l++) {
+          if (arr[i] + arr[j] + arr[k] + arr[l] === s) {
+            return [arr[i], arr[j], arr[k], arr[l]];
+          }
         }
       }
     }
   }
-  return result;
+  return [];
 }
