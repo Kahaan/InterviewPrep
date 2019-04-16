98f9ca6d41d06e09c3beb2b48d140ab923212592
Kahaan <kahaan.r.patel@gmail.com>
2019-04-16 16:10:29 -0700

    Algopractice

--- Arrays & Strings/fibsRecursive.js
+++ Arrays & Strings/fibsRecursive.js
@@ -1,14 +1,15 @@
-function fib(n) {
-  let fibs = [1, 1];
-
-  function helper(n) {
-    let last = fibs[fibs.length - 1];
-    let secondLast = fibs[fibs.length - 2];
-    if (fibs.length >= n) return;
-    fibs.push(last + secondLast);
-    helper(n);
-  }
-
-  helper(n);
-  return fibs[fibs.length - 1];
-}
+// function fib(n) {
+//   let fibs = [1, 1];
+//
+//   function helper(n) {
+//     let last = fibs[fibs.length - 1];
+//     let secondLast = fibs[fibs.length - 2];
+//     if (fibs.length >= n) return;
+//     fibs.push(last + secondLast);
+//     helper(n);
+//   }
+//
+//   helper(n);
+//   return fibs[fibs.length - 1];
+// }
+//
