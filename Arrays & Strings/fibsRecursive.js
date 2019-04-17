8cd440d7d30621997c3faf63b9954aaa7de2fafb
Kahaan <kahaan.r.patel@gmail.com>
2019-04-17 11:23:40 -0700

    Algopractice

--- Arrays & Strings/fibsRecursive.js
+++ Arrays & Strings/fibsRecursive.js
@@ -1,15 +1,14 @@
-// function fib(n) {
-//   let fibs = [1, 1];
-//
-//   function helper(n) {
-//     let last = fibs[fibs.length - 1];
-//     let secondLast = fibs[fibs.length - 2];
-//     if (fibs.length >= n) return;
-//     fibs.push(last + secondLast);
-//     helper(n);
-//   }
-//
-//   helper(n);
-//   return fibs[fibs.length - 1];
-// }
-//
+function fib(n) {
+  let fibs = [1, 1];
+
+  function helper(n) {
+    let last = fibs[fibs.length - 1];
+    let secondLast = fibs[fibs.length - 2];
+    if (fibs.length >= n) return;
+    fibs.push(last + secondLast);
+    helper(n);
+  }
+
+  helper(n);
+  return fibs[fibs.length - 1];
+}
---
+++ Arrays & Strings/maxSubarray.js
@@ -0,0 +1,14 @@
+function maxSubarr(arr, n) {
+  let maxSum = 0;
+  for (let i = 0; i < n; i++) {
+    maxSum += arr[i];
+  }
+  let tempSum = maxSum;
+  for (let i = n; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - n] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+// Any problem that requires some computation of consecutive subArr can likely match this pattern
