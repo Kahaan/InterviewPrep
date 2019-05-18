8b5e11823ada2040e5abe474fd1f2ad91fa7dcef
Kahaan <kahaan.r.patel@gmail.com>
2019-05-17 18:54:23 -0700

    algopractice

--- Arrays & Strings/triplets.js
+++ Arrays & Strings/triplets.js
@@ -1,7 +1,7 @@
 function threeNumberSum(array, targetSum) {
   let result = [];
   array = array.sort((a, b) => a - b);
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length - 2; i++) {
     let small = i + 1;
     let large = array.length - 1;
     let balance = targetSum - array[i];
@@ -9,7 +9,8 @@ function threeNumberSum(array, targetSum) {
     while (small < large) {
       if (array[small] + array[large] == balance) {
         result.push([array[i], array[small], array[large]]);
-        break;
+        small++;
+        large--;
       } else if (array[small] + array[large] > balance) {
         large--;
       } else if (array[small] + array[large] < balance) {
